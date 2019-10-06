236db895f391c87faee255dab0002dcbeea736bc
Danil Apsadikov <DonPonchik18@gmail.com>
2019-10-06 19:18:17 +0300

    HW02 added

---
+++ HW02/src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import com.fileloader.FileLoader;
+
+public class Test {
+    public static void main(String[] args) {
+        FileLoader fileLoader = new FileLoader();
+        fileLoader.load(args);
+    }
+}
